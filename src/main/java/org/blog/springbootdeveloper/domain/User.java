eff8d92eea3dd5a5129e62180c1e109bc561bc86
chahyunsoo <chahs1012@naver.com>
2023-07-02 00:13:29 +0900

    feat : Domain,Repository,Service 구현

---
+++ src/main/java/org/blog/springbootdeveloper/domain/User.java
@@ -0,0 +1,73 @@
+package org.blog.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+import java.util.Collection;
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String pasword;
+
+    @Override //권한 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getPassword() {
+        return pasword;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    //계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        //만료되었는지 확인용
+        return true;  //true이면 아직 만료되지 않았음
+    }
+
+    //계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        //계정 잠금 되었는지 확인용
+        return false; //true이면 아직 잠금되지 않았음
+    }
+
+    //패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        //패스워드가 만료되었는지 확인용
+        return true; //true이면 아직 만료되지 않았음
+    }
+
+    //계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        //계정이 사용 가능한지 확인용
+        return true; //true이면 아직 계정 사용 가능함
+    }
+}
---
+++ src/main/java/org/blog/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package org.blog.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.blog.springbootdeveloper.domain.User;
+import org.blog.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
